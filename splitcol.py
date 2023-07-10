8678535666a2844ee86545c76cf4d619193ae6e9
Bob Harold <bgithub@halfgoat.com>
2023-07-10 19:20:04 -0400

    split to separate files works

--- splitcol.py
+++ splitcol.py
@@ -10,22 +10,25 @@ def main():
     parser.add_argument('outfile',help='base name of output files')
     parser.add_argument('ext',help='extension of output files')
     args=parser.parse_args()
-    print(f'files {args.outfile}_COL.{args.ext}')
+    #print(f'files {args.outfile}_COL.{args.ext}')
     
     # read headers
     headers=[]
     reader=csv.reader(sys.stdin)
     for row in reader:
+        # remove any blank entries at end of row?
+        while len(row) and row[-1] == '':
+            row.pop(-1)
         headers.append(row)
-        print(row)
+        #print(row)
         if len(row) > 0:
             if row[0] == 'TIME':
-                print(f'found TIME')
+                #print(f'found TIME')
                 break
 
     # see how many signals
     numchans=len(row) - 1
-    print(f'numchans {numchans}')
+    #print(f'numchans {numchans}')
 
     # use list of dictionaries for the rest of the info on each signal, ordered by column number
     # signals = [
@@ -35,7 +38,7 @@ def main():
     signals=[]
     for column in range(1,numchans + 1):    # columns 1 to end, not column 0
         signalname=row[column]
-        print(f'signalname {signalname}')
+        #print(f'signalname {signalname}')
         
         # open the output files
         filename=f'{args.outfile}_{signalname}.{args.ext}'
@@ -47,19 +50,29 @@ def main():
 
     # write header lines to files
     for row in headers:
-        print(f'row is {row}')
+        #print(f'row is {row}')
         if len(row) == numchans + 1:
             for signal in signals:
                 column=signal['column']
                 signal['writer'].writerow([row[0],row[column]])
-                print(signal['filename'],row[0],row[column])
+                #print(signal['filename'],row[0],row[column])
         else:
             for signal in signals:
-                print(f'signal {signal}')
+                #print(f'signal {signal}')
                 writer=signal['writer']
-                print(f'row is {row}')
+                #print(f'row is {row}')
                 writer.writerow(row)
-                print(signal['filename'],row)
+                #print(signal['filename'],row)
+
+    # read and write data lines
+    for row in reader:
+        # remove any blank entries at end of row?
+        while len(row) and row[-1] == '':
+            row.pop(-1)
+        for signal in signals:
+            column=signal['column']
+            signal['writer'].writerow([row[0],row[column]])
+
 
 if __name__ == "__main__":
     main()
